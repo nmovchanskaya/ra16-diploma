a2fb3c0d8769c152513b2ea0686b1b0721e5d672
nika.movchanskaya <nika.movchanskaya@gmail.com>
2024-02-22 20:19:56 +0100

    data

--- src/entities/product/ui/ProductShort.tsx
+++ src/entities/product/ui/ProductShort.tsx
@@ -9,9 +9,13 @@ export const ProductShort = (props: {product: Product}) => {
                 <img src={product.images[0]}
                     className="card-img-top img-fluid" alt={product.title}/>
                 <div className="card-body">
-                    <p className="card-text">{product.title}</p>
-                    <p className="card-text">{product.price}</p>
-                    <NavLink to={"/catalog/" + product.id} className="btn btn-outline-primary">Order</NavLink>
+                    <div className="card-body__title">
+                        <p className="card-text">{product.title}</p>
+                        <p className="card-text">{product.price + "$"}</p>
+                    </div>
+                    <div className="card-body__btn">
+                        <NavLink to={"/catalog/" + product.id} className="btn btn-outline-primary">Order</NavLink>
+                    </div>
                 </div>
             </div>
         </div>
